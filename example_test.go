aea11e5a484ae535b5c3adee4f734623ffe252b7
Roger Clotet <roger.clotet@socialpoint.es>
2016-08-16 13:06:23 +0200

    Add basic example

---
+++ example_test.go
@@ -0,0 +1,40 @@
+package cqrs_test
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/rogerclotet/cqrs/argument"
+	"github.com/rogerclotet/cqrs/command"
+	"github.com/rogerclotet/cqrs/query"
+)
+
+func ExampleRegisterAndHandleCommandAndQuery() {
+	cr, _ := command.NewRegistry(
+		command.NewRegisteredCommand("increment", incrementHandler),
+	)
+
+	qr, _ := query.NewRegistry(
+		query.NewRegisteredQuery("value", valueHandler),
+	)
+
+	ctx := context.TODO()
+	_ = cr.Handle(ctx, command.New("increment", argument.Arguments{}))
+	_ = cr.Handle(ctx, command.New("increment", argument.Arguments{}))
+
+	v, _ := qr.Handle(ctx, query.New("value", argument.Arguments{}))
+	fmt.Println(v)
+
+	// Output: 2
+}
+
+var value int
+
+func incrementHandler(_ context.Context, _ argument.Arguments) error {
+	value++
+	return nil
+}
+
+func valueHandler(_ context.Context, _ argument.Arguments) (interface{}, error) {
+	return value, nil
+}
